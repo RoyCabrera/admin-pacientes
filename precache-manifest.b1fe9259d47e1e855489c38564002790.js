420f600a82442b17c57f4a0218b73a5ce26f1a4b
Roy Andy <roycabrera80@gmail.com>
2020-04-10 13:26:34 -0500

    Updates

---
+++ precache-manifest.b1fe9259d47e1e855489c38564002790.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5080914140191ec6606f934b8a5d1e27",
+    "url": "/admin-pacientes/index.html"
+  },
+  {
+    "revision": "c0681153389d5b5209b2",
+    "url": "/admin-pacientes/static/css/main.b6152572.chunk.css"
+  },
+  {
+    "revision": "a475d1670470b3dfe105",
+    "url": "/admin-pacientes/static/js/2.ad46ac13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/admin-pacientes/static/js/2.ad46ac13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0681153389d5b5209b2",
+    "url": "/admin-pacientes/static/js/main.1c7dac54.chunk.js"
+  },
+  {
+    "revision": "e694417c99a2a162f53f",
+    "url": "/admin-pacientes/static/js/runtime-main.fb0bf61f.js"
+  }
+]);
